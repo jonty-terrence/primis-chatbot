9b2af2fbf2d2179ebe138945e82a44c36e521553
Jonty-Buchan <jontyterrence@gmail.com>
2020-08-24 23:20:12 +1200

    updates API call to superagent process

--- myBot/src/actions/weather.js
+++ myBot/src/actions/weather.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import request from 'superagent'
+
 import { Text, Reply } from '@botonic/react'
 
-const res = await fetch('api.openweathermap.org/data/2.5/weather?q=auckland&appid=edb09865421b065e3746b106c40bcf1b')
-const user = await res.json()
+const forecastUrl = 'api.openweathermap.org/data/2.5/weather?q=Auckland&appid=edb09865421b065e3746b106c40bcf1b'
 
 export default class extends React.Component {
+
+    state = {}
+
+    componentDidMount () {
+        request.get(forecastUrl)
+        .then(res => {
+        const currentForecast = JSON.parse(res.body)
+        this.setState({ forecast: currentForecast })
+              })
+            }
 
   render() {
     return (
       <>
       <Text>
-          The weather in Auckland is {user.weather.main}, {user.weather.description}. 
+          The weather in Auckland is {this.state.forecast.visibility}. 
       </Text>
       </>
     )
